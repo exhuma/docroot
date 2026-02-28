5c0af92692dee4272047645323c2c2ede5d946d2
Michel Albert <michel@albert.lu>
2026-02-28 12:27:22 +0100

    linting

--- apps/frontend/src/plugins/index.ts
+++ apps/frontend/src/plugins/index.ts
@@ -1,19 +1,15 @@
-import router from '../router';
-import i18n from './i18n';
-/**
- * plugins/index.ts
- *
- * Automatically included in `./src/main.ts`
- */
-
 // Types
 import type { App } from 'vue'
+
+import router from '../router'
+
+import i18n from './i18n'
 
 // Plugins
 import vuetify from './vuetify'
 
 export function registerPlugins (app: App) {
- app.use(vuetify)
- app.use(i18n);
- app.use(router);
+  app.use(vuetify)
+  app.use(i18n)
+  app.use(router)
 }
